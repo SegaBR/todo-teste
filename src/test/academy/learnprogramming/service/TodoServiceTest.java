b7776e412b37ef75aa2731f6cbed8e61fe1c84c8
christofer sega <christofersega2@gmail.com>
2021-11-16 09:10:06 -0300

    first commit

---
+++ src/test/academy/learnprogramming/service/TodoServiceTest.java
@@ -0,0 +1,63 @@
+package academy.learnprogramming.service;
+
+import academy.learnprogramming.entity.Todo;
+import academy.learnprogramming.entity.User;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class TodoServiceTest {
+
+    @Inject
+    private User user;
+
+    @Inject
+    TodoService todoService;
+    Logger logger;
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "hello-todo.war")
+                .addPackage(Todo.class.getPackage())
+                .addPackage(TodoService.class.getPackage())
+                .addAsResource("persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        logger = Logger.getLogger(TodoServiceTest.class.getName());
+        user.setEmail("bla@bla.com");
+        user.setFullName("TEste");
+        user.setPassword("teste@1");
+            
+        todoService.saveUser(user);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+    @Test
+    public void saveUser() {
+           assertNotNull(user.getId());
+           logger.log(Level.INFO, user.getId().toString());
+           assertNotEquals("The user password is not same as hashed","teste@1", user.getPassword());
+           logger.log(Level.INFO, user.getPassword());
+    }
+}
